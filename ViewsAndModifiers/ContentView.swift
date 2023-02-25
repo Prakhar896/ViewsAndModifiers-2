6444100e9ccae046fe256eb3699e1dc0b96ce826
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-02-25 23:23:32 +0800

    completed views and modifiers project and challenge

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,15 +7,69 @@
 
 import SwiftUI
 
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+    
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct LargeBlueTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func largeBlueTitle() -> some View {
+        modifier(LargeBlueTitle())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello World!")
+//                .modifier(Title()) // Use the .modifier modifier to apply our custom Title view modifier
+                .titleStyle() // simpler version by extending the View protocol
+            Color.blue
+                .frame(width: 300, height: 200)
+                .watermarked(with: "Prakhar Trivedi Studios")
+            Text("My Big Blue Title")
+                .largeBlueTitle()
         }
-        .padding()
     }
 }
 
